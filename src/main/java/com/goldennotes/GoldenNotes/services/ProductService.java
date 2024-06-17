3cf5e0dd40d77008f5cac8edda6f5a6bb99d89a1
baykalgurbuz <ebaykalgurbuz@gmail.com>
2024-06-18 01:03:45 +0300

    application properties files have been changed

--- src/main/java/com/goldennotes/GoldenNotes/services/ProductService.java
+++ src/main/java/com/goldennotes/GoldenNotes/services/ProductService.java
@@ -3,9 +3,10 @@ package com.goldennotes.GoldenNotes.services;
 import com.goldennotes.GoldenNotes.entities.Product;
 import com.goldennotes.GoldenNotes.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
