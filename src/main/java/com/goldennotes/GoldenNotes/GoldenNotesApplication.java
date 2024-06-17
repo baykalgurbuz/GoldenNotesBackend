55421295ffa0c297aeba92ad083a3f470608977a
baykalgurbuz <ebaykalgurbuz@gmail.com>
2024-06-17 23:50:51 +0300

    Golden Notes Backend Side

---
+++ src/main/java/com/goldennotes/GoldenNotes/GoldenNotesApplication.java
@@ -0,0 +1,13 @@
+package com.goldennotes.GoldenNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goldennotes/GoldenNotes/GoldenNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldennotes.GoldenNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
